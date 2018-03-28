4a2a58d6ff78b03080f32eff77987619345d1595
teddywilson <hello@teddywilson.me>
2018-03-28 11:06:20 -0700

    adjusted comment

--- core/src/test/scala/blobstore/AbstractStoreTest.scala
+++ core/src/test/scala/blobstore/AbstractStoreTest.scala
@@ -253,8 +253,7 @@ trait AbstractStoreTest extends FlatSpec with MustMatchers with BeforeAndAfterAl
     test.unsafeRunSync()
   }
 
-  // TODO this doesn't test recursive directories. Once listRecursively() is implemented we
-  // can fix this
+  // TODO this doesn't test recursive directories. Once listRecursively() is implemented we can fix this
   it should "remove all should remove all files in a directory" in {
     val srcDir = dirPath("rm-dir-to-dir-src")
 
